5976bb98888de1c96ab74e199f8ddf40b8d4d96b
lchahua <62721646+edededd@users.noreply.github.com>
2022-06-01 16:14:49 -0400

    add test

--- src/test/java/testsoftware/TestApp.java
+++ src/test/java/testsoftware/TestApp.java
@@ -12,20 +12,21 @@ public class TestApp {
         User chahua = new User(29,3,2001);
         User chahua1 = new User(12,4,2004);
         User chahua2 = new User(1,6,2004);
-        User chahua3 = new User(2,6,2004);
+        User chahua3 = new User(2,7,1999);
         Consulta consulta = new Consulta(1,6,2022,chahua);
         Consulta consulta1 = new Consulta(1,6,2022,chahua1);
         Consulta consulta2 = new Consulta(1,6,2022,chahua2);
         Consulta consulta3 = new Consulta(1,5,2022,chahua2);
         Consulta consulta4 = new Consulta(4,3,2019,chahua);
         Consulta consulta5 = new Consulta(1,6,2022,chahua3);
-        Consulta consulta6 = new Consulta(1,6,2022,chahua3);
+        Consulta consulta6 = new Consulta(1,6,2020,chahua3);
         Assert.assertEquals(consulta.isOlder(), 1);
         Assert.assertEquals(consulta1.isOlder(), 1);
         Assert.assertEquals(consulta2.isOlder(), 1);
         Assert.assertEquals(consulta3.isOlder(), 0);
         Assert.assertEquals(consulta4.isOlder(), 0);
-        Assert.assertEquals(consulta5.isOlder(), 0);
+        Assert.assertEquals(consulta5.isOlder(), 1);
+        Assert.assertEquals(consulta6.isOlder(), 1);
 
     }
 
