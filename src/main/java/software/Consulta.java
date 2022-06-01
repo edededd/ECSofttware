5d4f025c9b148c65921a77cab1a5e136ec359a62
lchahua <62721646+edededd@users.noreply.github.com>
2022-06-01 19:43:22 -0400

    add test

--- src/main/java/software/Consulta.java
+++ src/main/java/software/Consulta.java
@@ -27,7 +27,10 @@ public class Consulta {
     int difMes= mes - user.getMes();
     int difDia= dia - user.getDia();
     String comment = "Haz colocado mal tu fecha de nacimiento, asegurate de que no sea despues de la fecha actual";
-    if(difAge<0 || (difAge==0 && difMes <0) || (difAge==0 && difMes==0 && difDia<0)){throw new DateTimeException(comment);}
+    if(difAge<0) {throw new DateTimeException(comment);}
+    if(difAge==0 && difMes<0) {throw new DateTimeException(comment);}
+    if(difAge==0 && difMes==0 && difDia<0) {throw new DateTimeException(comment);}
+    //if(difAge<0 || (difAge==0 && difMes <0) || (difAge==0 && difMes==0 && difDia<0)){throw new DateTimeException(comment);}
     }
 
    public void correctoFormato(){
