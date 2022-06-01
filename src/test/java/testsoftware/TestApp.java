3bf262a99f70e27e258cee42c99d38aec39de71a
lchahua <62721646+edededd@users.noreply.github.com>
2022-06-01 04:43:20 -0400

    fix errorss

--- src/test/java/testsoftware/TestApp.java
+++ src/test/java/testsoftware/TestApp.java
@@ -9,10 +9,18 @@ import software.Consulta;
 import software.User;
 public class TestApp {
     @Test
-    public void test(){
+    public void test1(){
         User chahua = new User(29,3,2001);
         Consulta consulta = new Consulta(1,6,2022,chahua);
         Assert.assertEquals(consulta.isOlder(), 1);
+
+    }
+
+    @Test
+    public void test2(){
+        User chahua = new User(29,3,2001);
+        Consulta consulta = new Consulta(4,3,2019,chahua);
+        Assert.assertEquals(consulta.isOlder(), 0);
 
     }
   
